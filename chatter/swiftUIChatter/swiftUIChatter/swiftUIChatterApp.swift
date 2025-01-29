2bba2363da4299edd5def18906244b926e2a3059
Amulya Parmar! TYG!! <parmar.amulya@gmail.com>
2025-01-29 08:50:28 -0500

    Add basic skeleton for Chatter TYG!!
    Sincerely grateful TYG!

---
+++ chatter/swiftUIChatter/swiftUIChatter/swiftUIChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIChatterApp.swift
+//  swiftUIChatter
+//
+//  Created by BlessedAmulya Parmar on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
