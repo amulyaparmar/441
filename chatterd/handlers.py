7925955dc101b7c7ab8e146b77eff1f7392be847
Gideon Shaked <gideonshaked@gmail.com>
2025-02-11 18:06:53 -0500

    add audio backend

--- chatterd/handlers.py
+++ chatterd/handlers.py
@@ -7,40 +7,72 @@ from starlette.responses import JSONResponse
 from typing import Optional
 from uuid import UUID
 
+
 @dataclass
 class Chatt:
     username: str
     message: str
+    audio: Optional[str] = None
+
 
 async def getchatts(request):
     try:
         async with main.server.pool.connection() as connection:
             async with connection.cursor() as cursor:
-                await cursor.execute('SELECT username, message, id, time FROM chatts ORDER BY time DESC;')
+                await cursor.execute(
+                    "SELECT username, message, id, time FROM chatts ORDER BY time DESC;"
+                )
                 return JSONResponse(jsonable_encoder(await cursor.fetchall()))
     except Exception as err:
-        print(f'{err=}')
-        return JSONResponse(f'{type(err).__name__}: {str(err)}', status_code = 500)
+        print(f"{err=}")
+        return JSONResponse(f"{type(err).__name__}: {str(err)}", status_code=500)
+
 
 async def postchatt(request):
     try:
         # loading json (not multipart/form-data)
         chatt = Chatt(**(await request.json()))
     except Exception as err:
-        print(f'{err=}')
-        return JSONResponse(f'Unprocessable entity: {str(err)}', status_code=422)
+        print(f"{err=}")
+        return JSONResponse(f"Unprocessable entity: {str(err)}", status_code=422)
 
     try:
         async with main.server.pool.connection() as connection:
             async with connection.cursor() as cursor:
-                await cursor.execute('INSERT INTO chatts (username, message, id) VALUES '
-                    '(%s, %s, gen_random_uuid());', (chatt.username, chatt.message))
+                await cursor.execute(
+                    "INSERT INTO chatts (username, message, id) VALUES "
+                    "(%s, %s, gen_random_uuid());",
+                    (chatt.username, chatt.message),
+                )
         return JSONResponse({})
     except StringDataRightTruncation as err:
-        print(f'Message too long: {str(err)}')
-        return JSONResponse(f'Message too long: {str(err)}', status_code = 400)
+        print(f"Message too long: {str(err)}")
+        return JSONResponse(f"Message too long: {str(err)}", status_code=400)
     except Exception as err:
-        print(f'{err=}')
-        return JSONResponse(f'{type(err).__name__}: {str(err)}', status_code = 500)
+        print(f"{err=}")
+        return JSONResponse(f"{type(err).__name__}: {str(err)}", status_code=500)
 
-        
+
+async def postaudio(request):
+    try:
+        # loading json (not multipart/form-data)
+        chatt = Chatt(**(await request.json()))
+    except Exception as err:
+        print(f"{err=}")
+        return JSONResponse(f"Unprocessable entity: {str(err)}", status_code=422)
+
+    try:
+        async with main.server.pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(
+                    "INSERT INTO chatts (username, message, id, audio) VALUES "
+                    "(%s, %s, gen_random_uuid(), %s);",
+                    (chatt.username, chatt.message, chatt.audio),
+                )
+        return JSONResponse({})
+    except StringDataRightTruncation as err:
+        print(f"Message too long: {str(err)}")
+        return JSONResponse(f"Message too long: {str(err)}", status_code=400)
+    except Exception as err:
+        print(f"{err=}")
+        return JSONResponse(f"{type(err).__name__}: {str(err)}", status_code=500)
--- chatterd/main.py
+++ chatterd/main.py
@@ -4,17 +4,23 @@ from psycopg_pool import AsyncConnectionPool
 from starlette.applications import Starlette
 from starlette.routing import Route
 
+
 @asynccontextmanager
 async def lifespan(server):
-    server.pool = AsyncConnectionPool("dbname=chatterdb user=chatter password=chattchatt host=localhost", open=False)
+    server.pool = AsyncConnectionPool(
+        "dbname=chatterdb user=chatter password=chattchatt host=localhost", open=False
+    )
     await server.pool.open()
     yield
     await server.pool.close()
 
+
 # must include the trailing '/'
 routes = [
-    Route('/getchatts/', handlers.getchatts, methods=['GET']),
-    Route('/postchatt/', handlers.postchatt, methods=['POST']),
+    Route("/getchatts/", handlers.getchatts, methods=["GET"]),
+    Route("/postchatt/", handlers.postchatt, methods=["POST"]),
+    Route("/getaudio/", handlers.getaudio, methods=["GET"]),
+    Route("/postaudio/", handlers.postaudio, methods=["POST"]),
 ]
 
 # must come after route definitions
